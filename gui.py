c12ff8def186705a19fc476e681f34477bcf44a0
Anand Mishra <a.anandmishra@hotmail.com>
2023-06-04 23:23:24 +0530

    added new features in add button implemented edit button
    Signed-off-by: Anand Mishra <a.anandmishra@hotmail.com>

--- gui.py
+++ gui.py
@@ -5,24 +5,41 @@ label = sg.Text("Type in A to-do")
 input_box = sg.InputText(tooltip ="Enter To-do", key="todo")
 add_button = sg.Button("Add")
 # add_action = sg.
+list_box = sg.Listbox(values= functions.get_todos(), key='todos',
+                      enable_events=True, size=[45,12])
+edit_button= sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label, input_box, add_button]],
+                   layout=[[label, input_box, add_button],[list_box,edit_button  ]],
                    font=('Helvetica', 10))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['todos'])
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
-
-
 
 
 window.close()
